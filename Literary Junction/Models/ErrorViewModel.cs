c8b5226345e130954f444bb9d4e67c34db094eab
abshuvo <anik58267@gmail.com>
2023-06-23 09:23:48 +0600

    Add project files.

---
+++ Literary Junction/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Literary_Junction.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
