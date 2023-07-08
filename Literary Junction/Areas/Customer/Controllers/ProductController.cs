8c8e3711d65783eb818702f98fa1de72568af6b4
abshuvo <anik58267@gmail.com>
2023-07-09 00:35:14 +0600

    I worked with ProductCrud and view model

---
+++ Literary Junction/Areas/Customer/Controllers/ProductController.cs
@@ -0,0 +1,128 @@
+﻿using LiteraryJunction.DataAccess.Data;
+using LiteraryJunction.DataAccess.Repository.IRepository;
+using LiteraryJunction.Models;
+using LiteraryJunction.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace Literary_Junction.Areas.Customer.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            
+            return View(objProductList);
+        }
+
+
+        //------------Create Method---------
+        public IActionResult Create()
+        {
+           
+            
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                }),
+                Product = new Product()
+            };
+            return View(productVM);
+        }
+        [HttpPost]
+        public IActionResult Create(ProductVM obj)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj.Product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Created Successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+
+        //========EDIT===================
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            //Product? productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id==id );
+            //Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+
+            if (productFromDb == null)
+            {
+                return NotFound(productFromDb);
+            }
+            return View(productFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Updated Successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+        //---------Delete Method------------
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            //Product? productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id==id );
+            //Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
+
+
+            if (productFromDb == null)
+            {
+                return NotFound(productFromDb);
+            }
+            return View(productFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Product Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
