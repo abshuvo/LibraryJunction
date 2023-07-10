e09cb986526c615e9e7a1ba90f07001a35bbd214
abshuvo <anik58267@gmail.com>
2023-07-10 23:59:19 +0600

    Combined Create and Edit named Upsert

--- Literary Junction/Areas/Customer/Controllers/ProductController.cs
+++ Literary Junction/Areas/Customer/Controllers/ProductController.cs
@@ -25,7 +25,7 @@ namespace Literary_Junction.Areas.Customer.Controllers
 
 
         //------------Create Method---------
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
            
             
@@ -38,59 +38,43 @@ namespace Literary_Junction.Areas.Customer.Controllers
                 }),
                 Product = new Product()
             };
-            return View(productVM);
+            if(id==null || id == 0)
+            {
+                //Create
+                return View(productVM);
+            }
+            else
+            {
+                //Update
+                productVM.Product=_unitOfWork.Product.Get(u=>u.Id==id);
+                return View(productVM);
+            } 
         }
         [HttpPost]
-        public IActionResult Create(ProductVM obj)
-        {
-            
+        public IActionResult Upsert(ProductVM productVM,IFormFile? file)
+        {  
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj.Product);
+                _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product Created Successfully";
                 return RedirectToAction("Index");
             }
-
-            return View();
+            else
+            {
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString(),
+                });
+                return View(productVM);
+            }    
         }
 
 
 
         //========EDIT===================
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            //Product? productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id==id );
-            //Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
-
-
-            if (productFromDb == null)
-            {
-                return NotFound(productFromDb);
-            }
-            return View(productFromDb);
-        }
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product Updated Successfully";
-                return RedirectToAction("Index");
-            }
-
-            return View();
-        }
-
+      
 
         //---------Delete Method------------
         public IActionResult Delete(int? id)
