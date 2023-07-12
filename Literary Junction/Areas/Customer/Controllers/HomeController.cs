0e2fddc43fa27ad2ae2fb3f3d8629eb554c75299
abshuvo <anik58267@gmail.com>
2023-07-13 00:03:51 +0600

    worked with home details

--- Literary Junction/Areas/Customer/Controllers/HomeController.cs
+++ Literary Junction/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using LiteraryJunction.Models;
+﻿using LiteraryJunction.DataAccess.Repository;
+using LiteraryJunction.DataAccess.Repository.IRepository;
+using LiteraryJunction.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +10,24 @@ namespace Literary_Junction.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork  _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product>productList=_unitOfWork.Product.GetAll(includeProperties:"Category");
+            return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product product=_unitOfWork.Product.Get(u=>u.Id== productId, includeProperties:"Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
--- Literary Junction/Areas/Customer/Controllers/ProductController.cs
+++ Literary Junction/Areas/Customer/Controllers/ProductController.cs
@@ -4,21 +4,24 @@ using LiteraryJunction.Models;
 using LiteraryJunction.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using NuGet.Protocol.Plugins;
 using System.Collections.Generic;
 
-namespace Literary_Junction.Areas.Customer.Controllers
+namespace Literary_Junction.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
-            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
             
             return View(objProductList);
         }
@@ -34,7 +37,7 @@ namespace Literary_Junction.Areas.Customer.Controllers
                 CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
                 {
                     Text = u.Name,
-                    Value = u.Id.ToString(),
+                    Value = u.Id.ToString()
                 }),
                 Product = new Product()
             };
@@ -52,10 +55,40 @@ namespace Literary_Junction.Areas.Customer.Controllers
         }
         [HttpPost]
         public IActionResult Upsert(ProductVM productVM,IFormFile? file)
-        {  
+        {
+            
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(productVM.Product);
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if(file!=null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    if(!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        var oldImagePath=Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+                        if(System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
+
+                if(productVM.Product.Id ==0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productVM.Product);
+                }
                 _unitOfWork.Save();
                 TempData["success"] = "Product Created Successfully";
                 return RedirectToAction("Index");
@@ -77,36 +110,32 @@ namespace Literary_Junction.Areas.Customer.Controllers
       
 
         //---------Delete Method------------
-        public IActionResult Delete(int? id)
+      
+        #region Api Calls
+       [HttpGet]
+        public IActionResult GetAll(int id)
         {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-            //Product? productFromDb1 = _db.Categories.FirstOrDefault(u => u.Id==id );
-            //Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault();
-
-
-            if (productFromDb == null)
-            {
-                return NotFound(productFromDb);
-            }
-            return View(productFromDb);
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = objProductList });
         }
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePost(int? id)
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
         {
-            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
-            if (obj == null)
+            var productToBeDeleted=_unitOfWork.Product.Get(u => u.Id==id);
+            if (productToBeDeleted == null)
             {
-                return NotFound();
+                return Json(new { success = false, message = "Error While Deleting" });
             }
-            _unitOfWork.Product.Remove(obj);
+            var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, productToBeDeleted.ImageUrl.TrimStart('\\'));
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+            _unitOfWork.Product.Remove(productToBeDeleted);
             _unitOfWork.Save();
-            TempData["success"] = "Product Deleted Successfully";
-            return RedirectToAction("Index");
+            return Json(new { success = true, message = "Delete Successful" });
         }
+        #endregion
     }
 }
